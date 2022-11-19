08eeafc84302f23ff860fae45f5414c9b9b5ff8b
Ben Stickelbrucks <ben.stickelbrucks@web.de>
2022-11-19 10:42:09 +0100

    implemented movie comments

--- backend/main.py
+++ backend/main.py
@@ -53,7 +53,7 @@ def movie_next(num=5):
 
 @app.route("/series/poster/<imdb_id>")
 def tv_poster(imdb_id):
-    new_id=get_id(imdb_id)
+    new_id = str(get_id(imdb_id))
     with urllib.request.urlopen(mdb_url + "tv/" + new_id + "/images" + "?api_key=" + mdb_key) as url:
         req = json.loads(url.read().decode())
         ret = req["posters"][0]["file_path"]
@@ -61,7 +61,13 @@ def tv_poster(imdb_id):
 
 @app.route("/movie/comments/<imdb_id>")
 def movie_comments(imdb_id):
-    pass
+    new_id = get_id(imdb_id)
+    with urllib.request.urlopen(mdb_url+ "movie/" + new_id + "/reviews" + "?api_key=" + mdb_key) as url:
+        req = json.loads(url.read().decode())
+        comments = []
+        for r in req["results"]:
+            comments.append(r["content"])
+    return {"comments": comments}
 
 @app.route("/series/comments/<imdb_id>")
 def tv_comments(imdb_id):
