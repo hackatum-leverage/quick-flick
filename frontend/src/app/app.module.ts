4cccf79ab430fe6b2d82fabd6499f4166dabcdcb
Joshua Pusch <41914279+ThePuscher@users.noreply.github.com>
2022-11-19 13:03:45 +0100

    Comment out ios mode

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -12,7 +12,7 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     IonicModule.forRoot({
-      mode: 'ios'
+      // mode: 'ios'
     }),
     AppRoutingModule
   ],
