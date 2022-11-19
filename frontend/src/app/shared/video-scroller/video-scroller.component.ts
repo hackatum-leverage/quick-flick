bd46fd1f4417d59992268dd03766a32a14b8a06b
Joshua Pusch <41914279+ThePuscher@users.noreply.github.com>
2022-11-19 16:24:47 +0100

    Add reviews

--- frontend/src/app/shared/video-scroller/video-scroller.component.ts
+++ frontend/src/app/shared/video-scroller/video-scroller.component.ts
@@ -1,36 +1,52 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
-import { Observable } from 'rxjs';
+import { AfterViewInit, Component, Input, OnInit, ViewChild } from '@angular/core';
 import { Offer } from 'src/app/models/offer.model';
 import { OffersService } from 'src/app/services/offers.service';
-import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-video-scroller',
   templateUrl: './video-scroller.component.html',
   styleUrls: ['./video-scroller.component.scss'],
 })
-export class VideoScrollerComponent implements OnInit {
+export class VideoScrollerComponent implements OnInit, AfterViewInit {
   @Input() type: "movie" | "series" = "movie";
 
   offers: Offer[] = [];
 
-  @ViewChild('iframe') iframe: ElementRef
+  review: string[] = [
+    'Lorem ipsum solor et dilor',
+    'liked it very much',
+    'Shit was bussin'
+  ]
+  reviewIndex = 0
+  timeout: any = null
 
   constructor(
-    private offersService: OffersService,
-    private http: HttpClient
+    private offersService: OffersService
   ) { }
 
   ngOnInit() {
     if (this.type === "movie") {
       this.offersService.getMovies().then((movies) => {
-        this.offers = movies;
-      });
+        this.offers = movies
+      })
     } else {
       this.offersService.getSeries().then((series) => {
-        this.offers = series;
-      });
+        this.offers = series
+      })
+    }
+  }
+
+  ngAfterViewInit() {
+    this.timeout = setInterval(() => {
+      this.nextReview()
+    }, 3000)
+  }
+
+  nextReview() {
+    if (this.reviewIndex < this.review.length - 1) {
+      this.reviewIndex++
+    } else {
+      this.timeout.invalidate()
     }
   }
 
