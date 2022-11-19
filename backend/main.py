938dd786d5507764e19211af9eace5254dbe4a00
Ben Stickelbrucks <ben.stickelbrucks@web.de>
2022-11-19 03:08:29 +0100

    refactorings regarding the IDS

--- backend/main.py
+++ backend/main.py
@@ -19,12 +19,22 @@ def test_json():
         "success": True
     }
 
-@app.route("/test/poster")
-def test_poster():
-    with urllib.request.urlopen(mdb_url + "movie/550?api_key=" + mdb_key) as url:
+@app.route("/test/poster/<imdb_id>")
+def test_poster(imdb_id="tt0137523"):
+    new_id = get_id(imdb_id)
+    with urllib.request.urlopen(mdb_url + "movie/" + new_id +"?api_key=" + mdb_key) as url:
         req = json.loads(url.read().decode())
         ret = req["poster_path"]
     return img_baseurl+size+ret
+
+def get_id(imdb_id):
+    with urllib.request.urlopen(mdb_url + "find/" + imdb_id + "?api_key=" + mdb_key + "&external_source=imdb_id") as url:
+        req = json.loads(url.read().decode())
+        if not req["tv_results"]:
+            ret = req["movie_results"][0]["id"]
+        else :
+            ret = req["tv_results"][0]["id"]
+    return str(ret)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
